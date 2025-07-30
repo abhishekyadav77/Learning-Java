de23f11b9527629c4c35aac6951c78ec434eeefc
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-07-30 23:20:39 +0530

    Push this code

---
+++ src/Literals.java
@@ -0,0 +1,14 @@
+// This is a Lecture 4 of CWH
+public class Literals {
+    public static void main(String[] args){
+        byte age = 45;
+        int age2 = 32;
+        String Name = "Abhishek";
+        char ch = 'A';
+        //here char is used for characters only for single characters only
+        System.out.println(age);
+        System.out.println(Name);
+        System.out.println(age2);
+    }
+}
+// Here literals are used in the above code
