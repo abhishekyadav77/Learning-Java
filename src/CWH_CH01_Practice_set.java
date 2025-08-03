22f8d8899bd589717cf859da593dfdc685e030aa
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-08-03 13:39:05 +0530

    Practice_Questions

---
+++ src/CWH_CH01_Practice_set.java
@@ -0,0 +1,81 @@
+// All codes all commented use it carefully
+/*
+import java.util.Scanner;
+public class CWH_CH01_Practice_set {
+    public static void main(String[] args){
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter 1st Number:");
+        float a = s1.nextFloat();
+        System.out.println("Enter Second Number:");
+        float b = s1.nextFloat();
+        System.out.println("Enter Third Number:");
+        float c = s1.nextFloat();
+        float sum = a+b+c;
+        System.out.println("The Sum Of three Numbers is " +sum);
+
+    }
+}
+*/
+
+
+//Practice Ques 2
+                              //QUESTION 3
+/*
+import java.util.Scanner;
+public class CWH_CH01_Practice_set{
+    public static void main(String[] args){
+        Scanner gpa = new Scanner(System.in);
+        System.out.println("GPA Calculator");
+        System.out.println("ENter first subject marks");
+        float a = gpa.nextFloat();
+        System.out.println("Enter Second subject marks");
+        float b = gpa.nextFloat();
+        System.out.println("Enter Third subject marks");
+        float c = gpa.nextFloat();
+        float sum = (float) (a+b+c);
+        float GPA = (float)(sum/300)*10;
+        System.out.println("The GPA of three Subjects is " +GPA);
+        gpa.close();
+    }
+}*/
+                        //QUESTION 4
+/*
+import java.util.Scanner;
+public class CWH_CH01_Practice_set{
+    public static void main(String[] args){
+        Scanner user = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String name = user.next();
+        System.out.println("Hello " +name + " Have a good day");
+        user.close();
+
+    }
+}*/
+
+/*
+import java.util.Scanner;
+public class CWH_CH01_Practice_set{
+    public static void main(String[] args){
+        Scanner KM = new Scanner(System.in);
+        System.out.println("Enter Number of Kilometeres");
+        double no_of_KM = KM.nextDouble();
+        double miles = 0.621371*no_of_KM;
+        System.out.println("No of mile in " + no_of_KM + "Kilometres is " + miles);
+        KM.close();
+    }
+
+}*/
+              //QUESTION 5
+/*
+import java.util.Scanner;
+public class CWH_CH01_Practice_set {
+
+    public static void main(String[] args){
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter a Number ");
+        boolean a = number.hasNextBoolean();
+        System.out.println(number.hasNextInt());
+
+    }
+}
+*/
