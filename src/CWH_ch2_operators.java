60cb730fb2068f0114bbbdf5fcffcf2ebecea240
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-08-03 22:21:53 +0530

    Operators

---
+++ src/CWH_ch2_operators.java
@@ -0,0 +1,13 @@
+public class CWH_ch2_operators {
+    public static void main(String[] args){
+      int a= 4;
+      int b = 5;
+      int c = a+b;
+        System.out.println(c); //use of arithmetic operators
+        System.out.println(4>7); //comparison operators
+        System.out.println(64>4 && 64>8); // logical operators are && logical or is ||
+    }
+}
+// the various operators are used to define the operators like arithmetic operators,
+//assignment operators, comparison operators logical operators, bitwise operators etc
+//all operators are used as per their requirement
