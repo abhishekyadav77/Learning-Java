5a3c550c7818118bc42efb3f0d5e90cf818250a4
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-08-04 18:57:03 +0530

    associativity

---
+++ src/CWh_09_operator_prescedence.java
@@ -0,0 +1,23 @@
+public class CWh_09_operator_prescedence {
+    public static void main(String[] args){
+        int a = 6*5-32/2;
+
+        int b = 60/5-34*2;
+        //12-17
+        //-56
+        System.out.println("The Calculated value is " +a);
+        System.out.println(b);
+    }
+}
+// prescedence and associativity
+// the prescedence of multiplication and division is equal so , we see the associativity...
+// associativity is left to right means that the b is calculated on the basis of their associativity left to right
+// division the multiplication
+                     // QUICK QUIZ
+    // k = x * y/2
+//  int x = 6;
+//int y = 1;
+ // System.out.println(k);
+// answer is 3
+// b*b -4ac /2a
+// b*b - (4*a*c)/(2*a)
