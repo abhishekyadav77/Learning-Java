5704a3aa20758d7e83a83899f5fd4505f12e1cc6
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-08-13 23:08:57 +0530

    String

---
+++ src/CWH_13_Strings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CWH_13_Strings {
+    public static void main(String args[]){
+       // String name = new String("Abhishek");
+        //String name = "Abhishek";
+        //System.out.print("The name is:");
+       // System.out.println(name);
+     //   int a = 9;
+       // float b= 7.094454f;
+       // System.out.printf("The value of a is %d and the value of b is %f", a,b);
+    Scanner sc = new Scanner(System.in);
+    String st = sc.nextLine();
+        System.out.println(st);
+    }
+
+}
