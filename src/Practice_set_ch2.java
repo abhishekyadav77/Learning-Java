981816d492c8d07aa92bb3961e6a6c041fe17fb9
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-08-21 21:12:11 +0530

    Practice Set

---
+++ src/Practice_set_ch2.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Practice_set_ch2 {
+    public static void main(String args[]){
+        String name = "Abhishek";
+        name= name.toLowerCase();
+        System.out.println(name);
+        //Problem 2
+        String text = "hello World";
+        System.out.println(text);
+        //Problem 3
+        String letter = "Dear <|name|>, THanks alot";
+        letter= letter.replace("<|name|>" , "Abhi");
+        System.out.println(letter);
+        //here replace is used as a string name
+         String replace = "To lower case";
+         replace = replace.replace(" " ,"_" );
+
+        System.out.println(replace);
+    }
+}
