50fdab6d944a506d0c95918454d84a7e41ceae6f
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-09-13 23:54:08 +0530

    do_while

--- src/Ex_2_RPS_GAME.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-public class Ex_2_RPS_GAME {
-    public static void main(String args[]){
-
-    }
-}
--- src/cwh_22_do_while.java
+++ src/cwh_22_do_while.java
@@ -1,5 +1,16 @@
 public class cwh_22_do_while {
     public static void main(String[] args){
+    int a=0;
+    while ((a<46)){
+        System.out.println(a);
+        a++;
+    }
+    /*while(a<5);
+    int b=0;
+do{
+    System.out.println(b);
+    b++;
+}while(b<46);*/
 
     }
 }
