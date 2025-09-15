d245403a8d8e85687dacc787de9c32c2daa29d0e
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-09-15 22:57:01 +0530

    Practice set 5

---
+++ src/cwh_25_practice_set.java
@@ -0,0 +1,33 @@
+public class cwh_25_practice_set {
+    public static void main(String[] args) {
+        //printing star pattern
+      /*  int n= 4;
+        for(int i=n; i>0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+        int sum = 0;
+        int n=3;
+        for (int i=0; i<n; i++) {
+            sum = sum + (2 * i);
+        }
+
+            System.out.println("Sum of even numbers is " );
+            System.out.println(sum);
+        // pRINTING TABLE OF 5
+        int n= 5;
+            for (int i=1; i<=10; i++){
+                System.out.printf("%d x %d \n",n,i, n*i);
+            }  */
+        int n= 5;
+        for (int i=10; i>=1; i--) {
+            System.out.printf("%d x %d \n", n, i, n * i);
+        }
+        // THE QUESTIONS IN A PRACTICE SET ARE LEFT FOR SOLVING ...THE SOLUTION IS PROVIDED LATER
+    }
+}
+
+
+
