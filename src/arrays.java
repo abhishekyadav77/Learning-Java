0fbcb07ed1612156a7e117383ab2ff48d94826e9
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-09-16 17:41:47 +0530

    Arrays

---
+++ src/arrays.java
@@ -0,0 +1,18 @@
+public class arrays {
+    public static void main(String[] args) {
+        //int [] marks = new int[5]; // declaration + memory allocation both in same line
+       // int[] marks;  // declares the variable or an array
+        //marks= new int[5];  // it allocates the memory
+        int [] marks = {100,070,80,71,98}; // declaration + memory allocation +initialization
+        // here the above lines is initialize the values in an array
+        // second method is below this line to initialize the values in an array
+//        marks[0] = 100;
+//        marks[1] = 68;
+//        marks[2] = 70;
+//        marks[3] = 95;
+//        marks[4] = 80;
+//        marks[3] = 70;
+        System.out.println(marks[4]);
+
+    }
+}
