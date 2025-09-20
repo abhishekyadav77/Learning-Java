3efcac4b0dd24f8b513f83b79efd2b5c077217f1
Abhishek Kumar Yadav <yadavvikky012@gmail.com>
2025-09-20 11:02:25 +0530

    Oops

---
+++ src/Java_Oops/oops_terminology.java
@@ -0,0 +1,27 @@
+package Java_Oops;
+
+public class oops_terminology {
+    public static void main(String[] args) {
+
+    }
+//    ABSTRACTION
+//    It hides the internal details
+//    and shows only essential details
+//    ENCAPSULATION
+//            The act of pushing various components together
+//    in a capsule
+//    ex
+//            laptop is a single entity with wifi + speaker + storage in a single box
+//    INHERITANCE
+//    the act of deriving new things from exixting things
+//            create new things from an exixting things
+//            ex Phone to Smartphone
+//            it represents the DRY principle
+//            DRY means Do not Repeat your self
+//            POLYMORPHISM
+//    one entity many forms
+//            smartphne to phone
+//    smartphone to calculator
+
+
+}
