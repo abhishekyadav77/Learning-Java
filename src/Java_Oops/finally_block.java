5996dec3f9a41a32760235aec0a2edee2199ab9b
abhishekyadav77 <yadavvikky012@gmail.com>
2025-09-30 21:33:31 +0530

    Finally block

---
+++ src/Java_Oops/finally_block.java
@@ -0,0 +1,44 @@
+package Java_Oops;
+
+public class finally_block {
+    public static int greet(){
+        try {
+
+            int a= 5;
+            int b = 0;
+            int c = a/b;
+            return c;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("This is the end of the program");
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+       int k = greet();
+        System.out.println(k);
+        int a = 7;
+        int b = 9;
+        while(true){
+            try{
+                System.out.println(a/b);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                break;
+            }
+            finally {
+                {
+                    System.out.println("I am finally for b " +b);
+                }
+                b--;
+            }
+        }
+    }
+}
+// finally block executes every time in program
+// it is not necessary that the try and catch both occur at same time ..the try and finnaly have occured
+// no problem executing
