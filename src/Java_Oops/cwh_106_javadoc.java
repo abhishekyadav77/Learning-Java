4533bed722330e6d20a816dc985a1e438e018085
abhishekyadav77 <yadavvikky012@gmail.com>
2025-10-08 11:13:31 +0530

    JavaDoc

---
+++ src/Java_Oops/cwh_106_javadoc.java
@@ -0,0 +1,26 @@
+
+
+
+package Java_Oops;
+
+/**
+ * This class is to demonstrate what java docis and how it is implemented in industry
+ * This is <i>italic</i> word <p> this is a new paragraph</p>
+ * @author  Abhishek (Abhishek Yadav)
+ * @version  0.7
+ * @since 2002
+ * @see java doc
+ */
+
+public class cwh_106_javadoc {
+    public void add (int a, int b){
+        System.out.println("The sum is "+a+b);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my main method");
+
+    }
+}
